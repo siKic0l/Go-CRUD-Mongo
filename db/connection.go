a3eb7995b87838575ff3db35d27a3659e2b1c965
siKic0l <aatel748@gmail.com>
2025-10-23 15:39:54 +0700

    prepare deploy

--- db/connection.go
+++ db/connection.go
@@ -13,24 +13,23 @@ import (
 var MongoClient *mongo.Client
 
 func MongoConnection() {
-	// Use the SetServerAPIOptions() method to set the version of the Stable API on the client
-	serverAPI := options.ServerAPI(options.ServerAPIVersion1)
-	opts := options.Client().ApplyURI(os.Getenv("MONGO_URI")).SetServerAPIOptions(serverAPI)
+	mongoURI := os.Getenv("MONGO_URI")
+	if mongoURI == "" {
+		panic("MONGO_URI tidak ditemukan di environment variable")
+	}
 
-	// Create a new client and connect to the server
+	serverAPI := options.ServerAPI(options.ServerAPIVersion1)
+	opts := options.Client().ApplyURI(mongoURI).SetServerAPIOptions(serverAPI)
+
 	client, err := mongo.Connect(context.TODO(), opts)
 	if err != nil {
 		panic(err)
 	}
-	MongoClient = client
 
-	// Send a ping to confirm a successful connection
 	if err := client.Database("admin").RunCommand(context.TODO(), bson.D{{"ping", 1}}).Err(); err != nil {
 		panic(err)
 	}
-	fmt.Println("Pinged your deployment. You successfully connected to MongoDB!")
-}
 
-func GetCollection(name string) *mongo.Collection {
-	return MongoClient.Database("gocrud_db").Collection(name)
+	fmt.Println("✅ Connected to MongoDB Atlas")
+	MongoClient = client
 }
